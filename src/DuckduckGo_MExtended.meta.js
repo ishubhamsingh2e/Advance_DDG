ea10f15863224a08471b2a966dc499d80a3a98ba
Shubham Singh <43581470+Sb2001nov@users.noreply.github.com>
2020-12-15 18:39:15 +0530

    Update DuckduckGo_MExtended.meta.js

--- src/DuckduckGo_MExtended.meta.js
+++ src/DuckduckGo_MExtended.meta.js
@@ -1,9 +1,8 @@
 // ==UserScript==
 // @name            DuckDuckAdvanceMenu
 // @description     Extends DuckDuckGo by adding a customizable list of additional search engines for making fast searches from other engines and make anime search faster.
-// @namespace       greasyfork.org/users/3926-jguer
-// @homepage        https://github.com/Jguer/DuckduckGo-Mextended
-// @icon            https://raw.githubusercontent.com/Jguer/DuckduckGo-Mextended/master/resources/large.png
+// @homepage        https://github.com/Sb2001nov/Advance_DDG/
+// @icon            https://raw.githubusercontent.com/Sb2001nov/Advance_DDG/main/resources/large.png
 // @include         *://duckduckgo.com/?q=*
 // @require         //ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // @grant           GM_getValue
